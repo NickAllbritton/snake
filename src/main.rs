7f3a91d19c5875fe83110ba8811e9df6f36f5f95
NickAllbritton <nallbritton@sycamores.indstate.edu>
2025-04-10 01:38:56 -0400

    Square black window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+
+
+fn main() -> Result<(), String> {
+
+    let scrn_width: u32 = 1000;
+    let scrn_height: u32 = 1000;
+
+    let sdl_context = sdl2::init()?;
+    let vid_subsystem = sdl_context.video()?;
+    let wnd = vid_subsystem.window("snake", scrn_width, scrn_height)
+        .build()
+        .unwrap();
+
+    let mut canvas = wnd.into_canvas()
+        .build()
+        .unwrap();
+
+    // Create board_view
+
+    // Create game_state
+    
+    let mut running: bool = true;
+    let mut event_queue = sdl_context.event_pump().unwrap();
+
+    while running {
+        for event in event_queue.poll_iter() {
+            match event {
+                sdl2::event::Event::Quit {..} => {
+                    running = false;
+                }
+                _ => {}
+            }
+        }
+
+
+        // Black background color
+        canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+        canvas.clear(); // Paint the background color
+    
+        canvas.present();
+    }
+    
+
+    Ok(())
 }
